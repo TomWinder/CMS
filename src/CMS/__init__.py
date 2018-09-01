4facb30a17f45944dcb517d7a4705a688c4b9a66
Ulvetanna <jds70@cam.ac.uk>
2018-09-01 14:52:50 +0100

    Alterations - Fixing Day Overlap Problem & Window offsets with normalisation of baselevel to 1.0. Version now change to beta stage 0.2

--- src/CMS/__init__.py
+++ src/CMS/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1"
+__version__ = "0.2"
 __description__ = "Coalescence Microseismic Scanning - CMScanning"
 __license__ = "GPL v3.0"
 __author__ = "Jonathan Smith, Julian Drew, Tom Hudson, and Tom Winder"
